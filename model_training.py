5febc0ab43f7cfbf24f36df1268cf572ce0b6a4c
Macaureus Mkoma <cbytesoftwares@gmail.com>
2025-07-23 08:45:56 +0300

    Create model_training.py

---
+++ model_training.py
@@ -0,0 +1,6 @@
+# Initialize and train Random Forest Regressor
+model = RandomForestRegressor(n_estimators=100, random_state=42)
+model.fit(X_train, y_train)
+
+# Predictions
+y_pred = model.predict(X_test)
